b5c9e897e2dea125518b49cfbca2778191321a73
serena booth <sbooth@mit.edu>
2019-11-13 11:15:48 -0500

    Progress with qm

---
+++ policy_summarization.py
@@ -0,0 +1,319 @@
+import json
+import numpy as np
+import time, datetime
+from qm import *
+
+class Policy(object):
+    predicate_list = None
+    predicate_count = -1
+    states = set()
+    actions = set()
+    transition_counts = {} # Dict[State str][Action str] = TransitionCount int
+    state_actions = {} # Dict[State str] = [Action str, Action str]
+
+    def __init__(self, filename):
+        self.load_from_json_abm(filename)
+
+    def load_from_json_abm (self, filename):
+        with open(filename) as json_file:
+            data = json.load(json_file)
+            self.predicate_list = data['fluents']
+            self.predicate_count = len(self.predicate_list)
+            for trace_id in data['traces']:
+
+                # Add actions to action set
+                # Add states to state set
+                # Add (state,action) pairs to transition_counts
+                for i in range(0, len(data['traces'][trace_id]['action'])):
+                    # Actions
+                    action = data['traces'][trace_id]['action'][i]
+                    self.actions.add(action)
+
+                    # States
+                    state = np.zeros(self.predicate_count, dtype=int)
+                    tmp_state = data['traces'][trace_id]['state'][i]
+                    for j in range(0, self.predicate_count):
+                        if self.predicate_list[j] in tmp_state:
+                            state[j] = 1
+                    state = str(state)
+                    self.states.add(state)
+
+                    # Transition Counts
+                    if (state, action) in self.transition_counts.keys():
+                        self.transition_counts[(state,action)] += 1
+                    else:
+                        self.transition_counts[(state, action)] = 1
+
+                    # Create dict of states with actions as entries
+                    if state in self.state_actions.keys():
+                        self.state_actions[state].add(action)
+                    else:
+                        self.state_actions[state] = set()
+                        self.state_actions[state].add(action)
+
+
+
+    #################################
+    '''
+    Question: What do you do?
+    Type: 'list_actions'
+    List NLP descriptions for each action
+    '''
+    ################################
+    def what_do_you_do(self):
+        return self.actions
+
+
+    #################################
+    '''
+    Question: When will you {action_list}?
+    Type: 'action_summary'
+    Find state clusters where action_list elements are the most probable action
+    '''
+    #################################
+
+    # Helper function for identify_action_clusters
+    def get_most_probable_action(self,state):
+        '''
+          state - State object
+          return - Tuple of max action (#, Action), action_counts dict
+        '''
+        counts = {}
+        max_count = (0, None)
+
+        for action in self.state_actions[state]:
+            count = self.transition_counts[(state,action)]
+            counts[action] = count
+            if count > max_count[0]:
+                max_count = (count, action)
+        # print "State " + str(state)
+        # print "Most probable action computation"
+        # print str(max_count)
+        # print str(counts)
+        return max_count, counts
+
+
+    def identify_action_clusters(self, state_list = None):
+        # Given graph, return a list of state clusters where particular actions are executed
+        # USE CASE: when will you do X?
+
+        # Find top-1 action for each state, remove all other edges from graph
+        # For each known action... make a state-cluster
+        #   * Iterate through all graph edges, looking for action label matching target action
+        #     * Remove matching edges from edge list and add origin state to current_action_cluster
+        #
+
+        if state_list is None: state_list = self.states
+
+        action_clusters = {} # Key=Action_name, Value=list of states
+
+        for state in state_list:
+            action, _ = self.get_most_probable_action(state)
+            if action[1] is None: continue
+            # TODO: Can set tiers of answers: "Always / Mostly / Sometimes / Rarely" corresponding to probability thresholds
+            action_str = action[1]
+            if action_str not in action_clusters: action_clusters[action_str] = []
+            action_clusters[action_str].append(state)
+
+        return action_clusters
+
+    def get_predicates(self, state):
+        predicates = []
+        state = list(state[1:-1].split(" "))
+        assert (len(state) == len(self.predicate_list))
+        print (state)
+        for char in state:
+            if char == "0":
+                predicates.append(False)
+            elif char == "1":
+                predicates.append(True)
+        return predicates, self.predicate_list
+
+    def perform_boolean_minimization(self, master_predicate_list, include_minterms, exclude_minterms):
+        '''
+        # Runs Quine-McCluskey algorithm on set of minterms
+        # Args:
+        #   master_predicate_list - list of possible predicates: e.g., [p1, p2, p3, ..., pn]
+        #   minterms - list of predicate truth values -- e.g., [ 10010, 01010, 01111 ] -- for each included state.
+        #     - len(minterms[0]) == len(master_predicate_list)
+        # Returns:
+        #   Minimized boolean expression: e.g., [ [True, None, None, False, None], [False, False, None, True, None] ]
+
+        # TODO: Possible optimization by minimizing the master_predicate_list size before running Quine-McCluskey
+        '''
+
+        print ('Include: %d, Exclude: %d' % (len(include_minterms), len(exclude_minterms)))
+        print ('Include: %s' % include_minterms)
+        print ('Exclude: %s' % exclude_minterms)
+
+        qm_minimization = qm(ones=include_minterms, zeros=exclude_minterms)
+
+        return qm_minimization, master_predicate_list
+
+    def solve_for_state_description_cover(self, state_list, total_state_list=None):
+        '''
+        # Solves for the best covering set of predicates that describes a list of states
+
+        ##### ARGS #####
+        state_list : List of states to include in description
+        total_state_list : List of states to consider in cover solution
+
+        ##### RETURNS #####
+        Returns predicate cover set, precision measures -- overstatement (#fp / #tp), understatement (#fn / #tp)
+        '''
+        overstatement = 0.
+        understatement = 0.
+
+        include_table = {}
+        exclude_table = {}
+
+        positive_minterms = []
+        negative_minterms = []
+
+        predicates_list = self.predicate_list
+        if total_state_list is None:
+            total_state_list = self.states
+        nonspec_is_negative = False
+
+        for state in total_state_list:
+            predicates, predicates_list = self.get_predicates(state)  # List of boolean values
+            val = 0
+            for idx in range(len(predicates)):
+                val |= predicates[idx] << idx
+
+            if state in state_list:
+                if val not in include_table:
+                    include_table[val] = 1
+                else:
+                    include_table[val] += 1
+            else:
+                if val not in exclude_table:
+                    exclude_table[val] = 1
+                else:
+                    exclude_table[val] += 1
+
+        print ("Include: %s" % include_table)
+        print ("Exclude: %s" % exclude_table)
+
+        # IDEA: Can use frequency of minterms in truth_table as measure for most important minterms
+
+        # # Collect all minterms
+        positive_minterms = include_table.keys()
+        for minterm in include_table:
+            # print "Added positive minterm: %s (%s)" % (str(minterm), b2s(minterm,len(predicates_list)))
+            if minterm in exclude_table:
+                # print "WARNING: positive minterm found in negative minterm table. Removing from negative minterm table."  # Minterms can't be positive and negative.
+                del exclude_table[minterm]
+
+
+            if nonspec_is_negative is True:
+                negative_minterms = [i for i in xrange(2**len(predicates))]
+                for minterm in positive_minterms:
+                    negative_minterms.remove(minterm)
+            else:
+                negative_minterms = exclude_table.keys()
+
+        time1 = datetime.datetime.now()
+        # Retrieve minimized formula describing state region:
+        # qm_minimization is string in [0,1,X]* that indexes into final_predicate_list
+        #   - first element in qm_minimization corresponds to last element of final_predicate_list
+        qm_minimization, qm_predicate_list = self.perform_boolean_minimization(predicates_list, positive_minterms, negative_minterms)
+        final_predicate_minimization = []
+        for minterm in qm_minimization:
+            final_predicate_minimization.append(minterm[::-1])
+        print ("Initial QM Minimization: %s" % qm_minimization[::-1])
+
+        final_predicate_list = qm_predicate_list
+        time2 = datetime.datetime.now()
+        time_diff = time2-time1
+
+        print ("Final QM Minimization: %s" % final_predicate_minimization)
+        print ("Predicates: %s" % str(predicates_list))
+
+        state_description = (final_predicate_minimization, final_predicate_list)
+        print (state_description)
+        #
+        # overstatement, understatement = self.evaluate_state_description(state_list, final_predicate_minimization, final_predicate_list)
+        #
+        # print "Overstatement %g, Understatement %g." % (overstatement, understatement)
+        return state_description, time_diff.total_seconds()
+
+    def solve_for_state_description(self, state_list, total_state_list=None):
+        '''
+        state_list - List of states to describe
+        total_state_list - List of states to consider when forming cover
+        '''
+
+        # Get predicate explanations for action region
+        cover, time_tmp = self.solve_for_state_description_cover(state_list, total_state_list)
+
+        explanations = []
+
+        values, predicate_funcs = cover
+        for clause in values:
+            clause_explanation = []
+            for idx, predicate_value in enumerate(clause):
+                if predicate_value == '1':   clause_explanation.append(self.predicate_list[idx])
+                elif predicate_value == '0':  clause_explanation.append("Not " + self.predicate_list[idx])
+
+        clause_summary = ' and '.join(clause_explanation)
+        if clause_summary not in explanations:
+            explanations.append(clause_summary)
+
+        return ' ---or--- '.join(explanations), time_tmp
+
+    def generate_action_cluster_descriptions(self, state_list, action_list=None, threshold=5):
+        '''
+        @param state_list list of states to include when summarizing action policy
+        @param threshold Maximum number of action clusters to include in summary
+        '''
+        action_clusters = self.identify_action_clusters(state_list)
+        '''
+        Build explanations for each state list
+        '''
+        descriptions = {}
+
+        # action clusters: action_clusters[action_str] = [state1, state2]
+        for action_type in action_clusters:
+            descriptions[action_type] = self.solve_for_state_description(state_list=action_clusters[action_type])
+
+        return descriptions
+
+    def describe_action_clusters(self, actions):
+        '''
+          @param graph Behavioral Graph
+          @param argument_text Comma-separated list of action names
+        '''
+        descriptions = {}
+
+        descriptions = self.generate_action_cluster_descriptions(state_list=self.states, action_list=actions, threshold=len(actions))
+
+        individual_descriptions = []
+        for action_name in descriptions:
+            individual_descriptions.append('I do %s when %s.' % (action_name, descriptions[action_name]))
+
+        return ' '.join(individual_descriptions)
+
+policy = Policy('blocksworld-new/p1.json')
+# print (policy.predicate_list)
+print (policy.identify_action_clusters())
+print (policy.describe_action_clusters(["put-down_b2"]))
+
+
+
+#################################
+'''
+Question: What will you do when {state_description}?
+Type: 'state_summary'
+Find action clusters within all states covered by state_description
+'''
+#################################
+
+
+#################################
+'''
+Question: Why aren't you doing {action}?
+Type: 'difference_summary'
+Find and describe states nearby the current state where {action} is the most probable choice.
+'''
+#################################
--- qm.py
+++ qm.py
@@ -27,119 +27,117 @@ import math
 
 
 def qm(ones=[], zeros=[], dc=[]):
-	'''Compute minimal two-level sum-of-products form.
-	Arguments are:
-		ones: iterable of integer minterms
-		zeros: iterable of integer maxterms
-		dc: iterable of integers specifying don't-care terms
+    '''Compute minimal two-level sum-of-products form.
+    Arguments are:
+    	ones: iterable of integer minterms
+    	zeros: iterable of integer maxterms
+    	dc: iterable of integers specifying don't-care terms
 
-	For proper operation, either (or both) the 'ones' and 'zeros'
-	parameters must be specified.  If one of these parameters is not
-	specified, it will be computed from the combination of the other
-	parameter and the optional 'dc' parameter.
+    For proper operation, either (or both) the 'ones' and 'zeros'
+    parameters must be specified.  If one of these parameters is not
+    specified, it will be computed from the combination of the other
+    parameter and the optional 'dc' parameter.
 
-	An assertion error will be raised if any terms are specified
-	in more than one argument, or if all three arguments are given
-	and not all terms are specified.'''
-
-	elts = max(max(ones or zeros or dc),
-		max(zeros or dc or ones),
-		max(dc or ones or zeros)) + 1
-	numvars = int(math.ceil(math.log(elts, 2)))
-	elts = 1 << numvars
-	all = set(b2s(i, numvars) for i in range(elts))
-	ones = set(b2s(i, numvars) for i in ones)
-	zeros = set(b2s(i, numvars) for i in zeros)
-	dc = set(b2s(i, numvars) for i in dc)
-	ones = ones or (all - zeros - dc)
-	zeros = zeros or (all - ones - dc)
-	dc = dc or (all - ones - zeros)
-	assert len(dc) + len(zeros) + len(ones) == len(dc | zeros | ones) == elts
-	primes = compute_primes(ones | dc, numvars)
-	return unate_cover(primes, ones)
-
+    An assertion error will be raised if any terms are specified
+    in more than one argument, or if all three arguments are given
+    and not all terms are specified.'''
+    elts = max(max(ones or zeros or dc), max(zeros or dc or ones), max(dc or ones or zeros)) + 1
+    numvars = int(math.ceil(math.log(elts, 2)))
+    elts = 1 << numvars
+    all = set(b2s(i, numvars) for i in range(elts))
+    ones = set(b2s(i, numvars) for i in ones)
+    zeros = set(b2s(i, numvars) for i in zeros)
+    dc = set(b2s(i, numvars) for i in dc)
+    ones = ones or (all - zeros - dc)
+    zeros = zeros or (all - ones - dc)
+    dc = dc or (all - ones - zeros)
+    assert len(dc) + len(zeros) + len(ones) == len(dc | zeros | ones) == elts
+    primes = compute_primes(ones | dc, numvars)
+    print ("primes computed!")
+    return unate_cover(primes, ones)
 
 def unate_cover(primes, ones):
-	'''Return the minimal cardinality subset of primes covering all ones.
+    '''Return the minimal cardinality subset of primes covering all ones.
 
-	Exhaustive for now.  Feel free to replace this with an efficient unate
-	covering problem solver.'''
+    Exhaustive for now.  Feel free to replace this with an efficient unate
+    covering problem solver.'''
 
-	primes = list(primes)
-	cs = min((bitcount(b2s(cubesel, len(primes))), cubesel)
-		for cubesel in range(1 << len(primes))
-		if is_full_cover(active_primes(cubesel, primes), ones))[1]
-	return active_primes(cs, primes)
+    primes = list(primes)
+    cs = min((bitcount(b2s(cubesel, len(primes))), cubesel)
+        for cubesel in range(1 << len(primes))
+        if is_full_cover(active_primes(cubesel, primes), ones))[1]
+    return active_primes(cs, primes)
 
 
 def active_primes(cubesel, primes):
-	'''Return the primes selected by the cube selection integer.'''
-	return [prime for used, prime in
-		zip(map(int, b2s(cubesel, len(primes))), primes) if used]
+    '''Return the primes selected by the cube selection integer.'''
+    return [prime for used, prime in
+        zip(map(int, b2s(cubesel, len(primes))), primes) if used]
 
 
 def is_full_cover(all_primes, ones):
-	'''Return a bool: Does the set of primes cover all minterms?'''
-	return min([max([is_cover(p, o) for p in all_primes] + [False])
-		for o in ones] + [True])
+    '''Return a bool: Does the set of primes cover all minterms?'''
+    return min([max([is_cover(p, o) for p in all_primes] + [False])
+        for o in ones] + [True])
 
 
 def is_cover(prime, one):
-	'''Return a bool: Does the prime cover the minterm?'''
-	return min([p == 'X' or p == o for p, o in zip(prime, one)] + [True])
+    '''Return a bool: Does the prime cover the minterm?'''
+    return min([p == 'X' or p == o for p, o in zip(prime, one)] + [True])
 
 
 def compute_primes(cubes, vars):
-	'''Compute primes for the given set of cubes and variable count.'''
-	sigma = [set(i for i in cubes if bitcount(i) == v)
-		for v in range(vars + 1)]
-	primes = set()
-	while sigma:
-		nsigma = []
-		redundant = set()
-		for c1, c2 in zip(sigma[:-1], sigma[1:]):
-			nc = set()
-			for a in c1:
-				for b in c2:
-					m = merge(a, b)
-					if m:
-						nc.add(m)
-						redundant |= set([a, b])
-			nsigma.append(nc)
-		primes |= set(c for cubes in sigma for c in cubes) - redundant
-		sigma = nsigma
-	return primes
+    '''Compute primes for the given set of cubes and variable count.'''
+    sigma = [set(i for i in cubes if bitcount(i) == v)
+        for v in range(vars + 1)]
+    primes = set()
+    while sigma:
+        nsigma = []
+        redundant = set()
+        for c1, c2 in zip(sigma[:-1], sigma[1:]):
+            nc = set()
+            for a in c1:
+                for b in c2:
+                    m = merge(a, b)
+                    if m:
+                        nc.add(m)
+                        redundant |= set([a, b])
+            print ("executing")
+            nsigma.append(nc)
+        primes |= set(c for cubes in sigma for c in cubes) - redundant
+        sigma = nsigma
+    return primes
 
 
 def bitcount(s):
-	'''Return the sum of on bits in s.'''
-	return sum(b == '1' for b in s)
+    '''Return the sum of on bits in s.'''
+    return sum(b == '1' for b in s)
 
 
 def b2s(i, vars):
-	'''Convert from an integer to a binary string.'''
-	s = ''
-	for k in range(vars):
-		s = ['0', '1'][i & 1] + s
-		i >>= 1
-	return s
+    '''Convert from an integer to a binary string.'''
+    s = ''
+    for k in range(vars):
+        s = ['0', '1'][i & 1] + s
+        i >>= 1
+    return s
 
 
 def merge(i, j):
-	'''Return cube merge.  'X' is don't-care.  'None' if merge impossible.'''
-	s = ''
-	dif_cnt = 0
-	for a, b in zip(i, j):
-		if (a == 'X' or b == 'X') and a != b:
-			return None
-		elif a != b:
-			dif_cnt += 1
-			s += 'X'
-		else:
-			s += a
-		if dif_cnt > 1:
-			return None
-	return s
+    '''Return cube merge.  'X' is don't-care.  'None' if merge impossible.'''
+    s = ''
+    dif_cnt = 0
+    for a, b in zip(i, j):
+        if (a == 'X' or b == 'X') and a != b:
+            return None
+        elif a != b:
+            dif_cnt += 1
+            s += 'X'
+        else:
+            s += a
+        if dif_cnt > 1:
+            return None
+    return s
 
 
 if __name__ == '__main__':
