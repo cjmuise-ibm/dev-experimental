bedfa50e00d3b89f336050d2053b71b635421133
Serena Booth <sbooth@Mit.edu>
2019-11-18 05:26:10 -0500

    Halfway to new QM solver

--- call_summarization.sh
+++ call_summarization.sh
@@ -1,6 +1,6 @@
 TIMEOUT=3600
 
-# python3 policy_summarization.py --filename domains/blocksworld-new/p1.json --timeout $TIMEOUT > /dev/tty
+python3 policy_summarization.py --filename domains/blocksworld-new/p1.json --timeout $TIMEOUT > /dev/tty
 # python3 policy_summarization.py --filename domains/blocksworld-new/p2.json --timeout $TIMEOUT > /dev/tty
 # python3 policy_summarization.py --filename domains/blocksworld-new/p3.json --timeout $TIMEOUT > /dev/tty
 
@@ -12,7 +12,7 @@ python3 policy_summarization.py --filename domains/elevators/p1.json --timeout $
 # python3 policy_summarization.py --filename domains/elevators/p2.json --timeout $TIMEOUT > /dev/tty
 # python3 policy_summarization.py --filename domains/elevators/p3.json --timeout $TIMEOUT > /dev/tty
 
-# python3 policy_summarization.py --filename domains/tiny-triangle-tireworld/p1.json --timeout $TIMEOUT > /dev/tty
+python3 policy_summarization.py --filename domains/tiny-triangle-tireworld/p1.json --timeout $TIMEOUT > /dev/tty
 # python3 policy_summarization.py --filename domains/tiny-triangle-tireworld/p2.json --timeout $TIMEOUT > /dev/tty
 # python3 policy_summarization.py --filename domains/tiny-triangle-tireworld/p3.json --timeout $TIMEOUT > /dev/tty
 
--- qm.py
+++ qm.py
@@ -1,178 +1,288 @@
 #!/usr/bin/env python
 
-'''Quine-McCluskey two-level logic minimization method.
+"""
+Copyright (c) 2012 George Prekas <prekgeo@yahoo.com>
 
-Copyright 2008, Robert Dick <dickrp@eecs.umich.edu> with improvements
-from Pat Maupin <pmaupin@gmail.com>.
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-Routines to compute the optimal sum of products implementation from sets of
-don't-care terms, minterms, and maxterms.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-Command-line usage example:
-  qm.py -o1,2,5 -d0,7
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
-Library usage example:
-  import qm
-  print qm.qm(ones=[1, 2, 5], dc=[0, 7])
+"""
+This class implements the Quine-McCluskey algorithm for minimization of boolean
+functions.
 
-Please see the license file for legal information.'''
+Based on code from Robert Dick <dickrp@eecs.umich.edu> and Pat Maupin
+<pmaupin@gmail.com>. Most of the original code was re-written for performance
+reasons.
 
+>>> qm = QM(['A','B'])
 
-__version__ = '0.2'
-__author__ = 'Robert Dick'
-__author_email__ = 'dickrp@eecs.umich.edu'
+>>> qm.get_function(qm.solve([],[])[1])
+'0'
+>>> qm.get_function(qm.solve([1,3],[0,2])[1])
+'1'
+>>> qm.get_function(qm.solve([0,1,2,3],[])[1])
+'1'
+>>> qm.get_function(qm.solve([3],[])[1])
+'(A AND B)'
+>>> qm.get_function(qm.solve([0],[])[1])
+'((NOT A) AND (NOT B))'
+>>> qm.get_function(qm.solve([1,3],[])[1])
+'A'
+>>> qm.get_function(qm.solve([1],[3])[1])
+'A'
+>>> qm.get_function(qm.solve([2,3],[])[1])
+'B'
+>>> qm.get_function(qm.solve([0,2],[])[1])
+'(NOT A)'
+>>> qm.get_function(qm.solve([0,1],[])[1])
+'(NOT B)'
+>>> qm.get_function(qm.solve([1,2,3],[])[1])
+'(A OR B)'
+>>> qm.get_function(qm.solve([0,1,2],[])[1])
+'((NOT B) OR (NOT A))'
+"""
 
+class QM:
+  def __init__(self, variables):
+    """
+    Initialize the Quine-McCluskey solver.
 
-import math
+    variables: a list of strings that are the names of the variables used in
+    the boolean functions
+    """
 
+    self.variables = variables
+    self.numvars = len(variables)
 
-def qm(ones=[], zeros=[], dc=[]):
-    '''Compute minimal two-level sum-of-products form.
-    Arguments are:
-    	ones: iterable of integer minterms
-    	zeros: iterable of integer maxterms
-    	dc: iterable of integers specifying don't-care terms
+  def solve(self, ones, dc):
+    """
+    Executes the Quine-McCluskey algorithm and returns its results.
 
-    For proper operation, either (or both) the 'ones' and 'zeros'
-    parameters must be specified.  If one of these parameters is not
-    specified, it will be computed from the combination of the other
-    parameter and the optional 'dc' parameter.
+    ones: a list of indices for the minterms for which the function evaluates
+    to 1
+    dc: a list of indices for the minterms for which we do not care about the
+    function evaluation
 
-    An assertion error will be raised if any terms are specified
-    in more than one argument, or if all three arguments are given
-    and not all terms are specified.'''
-    elts = max(max(ones or zeros or dc), max(zeros or dc or ones), max(dc or ones or zeros)) + 1
-    numvars = int(math.ceil(math.log(elts, 2)))
-    elts = 1 << numvars
-    all = set(b2s(i, numvars) for i in range(elts))
-    ones = set(b2s(i, numvars) for i in ones)
-    zeros = set(b2s(i, numvars) for i in zeros)
-    dc = set(b2s(i, numvars) for i in dc)
-    ones = ones or (all - zeros - dc)
-    zeros = zeros or (all - ones - dc)
-    dc = dc or (all - ones - zeros)
-    assert len(dc) + len(zeros) + len(ones) == len(dc | zeros | ones) == elts
-    primes = compute_primes(ones | dc, numvars)
-    return unate_cover(primes, ones)
+    returns: a tuple a,b; a is the complexity of the result and b is a list of
+    minterms which is the minified boolean function expressed as a sum of
+    products
+    """
 
-def unate_cover(primes, ones):
-    '''Return the minimal cardinality subset of primes covering all ones.
+    # Handle special case for functions that always evaluate to True or False.
+    if len(ones) == 0:
+      return 0,'0'
+    if len(ones) + len(dc) == 1<<self.numvars:
+      return 0,'1'
 
-    Exhaustive for now.  Feel free to replace this with an efficient unate
-    covering problem solver.'''
+    primes = self.compute_primes(ones + dc)
+    return self.unate_cover(list(primes), ones)
 
-    primes = list(primes)
-    cs = min((bitcount(b2s(cubesel, len(primes))), cubesel)
-        for cubesel in range(1 << len(primes))
-        if is_full_cover(active_primes(cubesel, primes), ones))[1]
-    return active_primes(cs, primes)
+  def compute_primes(self, cubes):
+    """
+    Find all prime implicants of the function.
 
+    cubes: a list of indices for the minterms for which the function evaluates
+    to 1 or don't-care.
+    """
 
-def active_primes(cubesel, primes):
-    '''Return the primes selected by the cube selection integer.'''
-    return [prime for used, prime in
-        zip(map(int, b2s(cubesel, len(primes))), primes) if used]
+    sigma = []
+    for i in range(self.numvars+1):
+      sigma.append(set())
+    for i in cubes:
+      sigma[bitcount(i)].add((i,0))
 
-
-def is_full_cover(all_primes, ones):
-    '''Return a bool: Does the set of primes cover all minterms?'''
-    return min([max([is_cover(p, o) for p in all_primes] + [False])
-        for o in ones] + [True])
-
-
-def is_cover(prime, one):
-    '''Return a bool: Does the prime cover the minterm?'''
-    return min([p == 'X' or p == o for p, o in zip(prime, one)] + [True])
-
-
-def compute_primes(cubes, vars):
-    '''Compute primes for the given set of cubes and variable count.'''
-    sigma = [set(i for i in cubes if bitcount(i) == v)
-        for v in range(vars + 1)]
     primes = set()
     while sigma:
-        nsigma = []
-        redundant = set()
-        for c1, c2 in zip(sigma[:-1], sigma[1:]):
-            nc = set()
-            for a in c1:
-                for b in c2:
-                    m = merge(a, b)
-                    if m:
-                        nc.add(m)
-                        redundant |= set([a, b])
-            nsigma.append(nc)
-        primes |= set(c for cubes in sigma for c in cubes) - redundant
-        sigma = nsigma
+      nsigma = []
+      redundant = set()
+      for c1, c2 in zip(sigma[:-1], sigma[1:]):
+        nc = set()
+        for a in c1:
+          for b in c2:
+            m = merge(a, b)
+            if m != None:
+              nc.add(m)
+              redundant |= set([a, b])
+        nsigma.append(nc)
+      primes |= set(c for cubes in sigma for c in cubes) - redundant
+      sigma = nsigma
     return primes
 
+  def unate_cover(self, primes, ones):
+    """
+    Use the prime implicants to find the essential prime implicants of the
+    function, as well as other prime implicants that are necessary to cover
+    the function. This method uses the Petrick's method, which is a technique
+    for determining all minimum sum-of-products solutions from a prime implicant
+    chart.
 
-def bitcount(s):
-    '''Return the sum of on bits in s.'''
-    return sum(b == '1' for b in s)
+    primes: the prime implicants that we want to minimize.
+    ones: a list of indices for the minterms for which we want the function to
+    evaluate to 1.
+    """
 
+    chart = []
+    for one in ones:
+      column = []
+      for i in range(len(primes)):
+        if (one & (~primes[i][1])) == primes[i][0]:
+          column.append(i)
+      chart.append(column)
 
-def b2s(i, vars):
-    '''Convert from an integer to a binary string.'''
-    s = ''
-    for k in range(vars):
-        s = ['0', '1'][i & 1] + s
-        i >>= 1
-    return s
+    covers = []
+    if len(chart) > 0:
+      covers = [set([i]) for i in chart[0]]
+    for i in range(1,len(chart)):
+      new_covers = []
+      for cover in covers:
+        for prime_index in chart[i]:
+          x = set(cover)
+          x.add(prime_index)
+          append = True
+          for j in range(len(new_covers)-1,-1,-1):
+            if x <= new_covers[j]:
+              del new_covers[j]
+            elif x > new_covers[j]:
+              append = False
+          if append:
+            new_covers.append(x)
+      covers = new_covers
 
+    min_complexity = 99999999
+    for cover in covers:
+      primes_in_cover = [primes[prime_index] for prime_index in cover]
+      complexity = self.calculate_complexity(primes_in_cover)
+      if complexity < min_complexity:
+        min_complexity = complexity
+        result = primes_in_cover
+
+    return min_complexity,result
+
+  def calculate_complexity(self, minterms):
+    """
+    Calculate the complexity of the given function. The complexity is calculated
+    based on the following rules:
+      A NOT gate adds 1 to the complexity.
+      A n-input AND or OR gate adds n to the complexity.
+
+    minterms: a list of minterms that form the function
+
+    returns: an integer that is the complexity of the function
+
+    >>> qm = QM(['A','B','C'])
+
+    >>> qm.calculate_complexity([(1,6)])
+    0
+    >>> qm.calculate_complexity([(0,6)])
+    1
+    >>> qm.calculate_complexity([(3,4)])
+    2
+    >>> qm.calculate_complexity([(7,0)])
+    3
+    >>> qm.calculate_complexity([(1,6),(2,5),(4,3)])
+    3
+    >>> qm.calculate_complexity([(0,6),(2,5),(4,3)])
+    4
+    >>> qm.calculate_complexity([(0,6),(0,5),(4,3)])
+    5
+    >>> qm.calculate_complexity([(0,6),(0,5),(0,3)])
+    6
+    >>> qm.calculate_complexity([(3,4),(7,0),(5,2)])
+    10
+    >>> qm.calculate_complexity([(1,4),(7,0),(5,2)])
+    11
+    >>> qm.calculate_complexity([(2,4),(7,0),(5,2)])
+    11
+    >>> qm.calculate_complexity([(0,4),(7,0),(5,2)])
+    12
+    >>> qm.calculate_complexity([(0,4),(0,0),(5,2)])
+    15
+    >>> qm.calculate_complexity([(0,4),(0,0),(0,2)])
+    17
+    """
+
+    complexity = len(minterms)
+    if complexity == 1:
+      complexity = 0
+    mask = (1<<self.numvars)-1
+    for minterm in minterms:
+      masked = ~minterm[1] & mask
+      term_complexity = bitcount(masked)
+      if term_complexity == 1:
+        term_complexity = 0
+      complexity += term_complexity
+      complexity += bitcount(~minterm[0] & masked)
+
+    return complexity
+
+  def get_function(self, minterms):
+    """
+    Return in human readable form a sum of products function.
+
+    minterms: a list of minterms that form the function
+
+    returns: a string that represents the function using operators AND, OR and
+    NOT.
+    """
+
+    if isinstance(minterms,str):
+      return minterms
+
+    def parentheses(glue, array):
+      if len(array) > 1:
+        return ''.join(['(',glue.join(array),')'])
+      else:
+        return glue.join(array)
+
+    or_terms = []
+    for minterm in minterms:
+      and_terms = []
+      for j in range(len(self.variables)):
+        if minterm[0] & 1<<j:
+          and_terms.append(self.variables[j])
+        elif not minterm[1] & 1<<j:
+          and_terms.append('(NOT %s)' % self.variables[j])
+      or_terms.append(parentheses(' AND ', and_terms))
+    return parentheses(' OR ', or_terms)
+
+def bitcount(i):
+  """ Count set bits of the input. """
+
+  res = 0
+  while i > 0:
+    res += i&1
+    i>>=1
+  return res
+
+def is_power_of_two_or_zero(x):
+  """
+  Determine if an input is zero or a power of two. Alternative, determine if an
+  input has at most 1 bit set.
+  """
+
+  return (x & (~x + 1)) == x
 
 def merge(i, j):
-    '''Return cube merge.  'X' is don't-care.  'None' if merge impossible.'''
-    s = ''
-    dif_cnt = 0
-    for a, b in zip(i, j):
-        if (a == 'X' or b == 'X') and a != b:
-            return None
-        elif a != b:
-            dif_cnt += 1
-            s += 'X'
-        else:
-            s += a
-        if dif_cnt > 1:
-            return None
-    return s
+  """ Combine two minterms. """
 
-
-if __name__ == '__main__':
-	from optparse import *
-	from sys import *
-
-	def main():
-		options = [
-			Option('-d', '--dontcares', dest='dc', default='',
-				help='comma-separated don\'t-cares', metavar='D'),
-			Option('-o', '--ones', dest='ones', default='',
-				help='comma-separated ones', metavar='O'),
-			Option('-z', '--zeros', dest='zeros', default='',
-				help='comma-separated zeros', metavar='Z')
-		]
-
-		f = IndentedHelpFormatter()
-		def raw_format(s): return s + '\n'
-		f.format_description = raw_format
-
-		optparser = OptionParser(description=__doc__, formatter=f)
-		optparser.add_options(options)
-		opts, args = optparser.parse_args()
-		if len(argv) == 1 or args:
-			optparser.print_help()
-			exit()
-
-		opts.dc = [int(i) for i in opts.dc.split(',') if i]
-		opts.ones = [int(i) for i in opts.ones.split(',') if i]
-		opts.zeros = [int(i) for i in opts.zeros.split(',') if i]
-
-		soln = qm(dc=opts.dc, ones=opts.ones, zeros=opts.zeros)
-		if len(soln) == 0:
-			stdout.write('contradiction\n')
-		elif len(soln) == 1 and soln[0].count('X') == len(soln[0]):
-			stdout.write('tautology\n')
-		else:
-			stdout.write(' '.join(soln) + '\n')
-
-	main()
+  if i[1] != j[1]:
+    return None
+  y = i[0] ^ j[0]
+  if not is_power_of_two_or_zero(y):
+    return None
+  return (i[0] & j[0],i[1]|y)
